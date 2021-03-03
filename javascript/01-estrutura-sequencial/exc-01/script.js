baff743dbd649f635b193fdbfeaaa18b3e8b8b03
Samuel Cipriano <samciprianoh@gmail.com>
2021-03-02 22:32:11 -0300

    Atualizaçao geral de pastas e arquivos

---
+++ javascript/01-estrutura-sequencial/exc-01/script.js
@@ -0,0 +1,10 @@
+function calcula(){
+   let n1, n2, result
+
+   n1 = Number(document.getElementById('n1').value)
+   n2 = Number(document.getElementById('n2').value)
+
+   result = n1 - n2
+   
+   document.getElementById('result').innerHTML = `${n1} - ${n2} = ${result}`
+}
